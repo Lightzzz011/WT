23091e6e01ef55f8f1bcdc5b44f6f0e9a755ba7b
sai koluguri <luckyizdone@gmail.com>
2025-10-27 11:49:24 +0530

    a

---
+++ classarmstrong.java
@@ -0,0 +1,26 @@
+class armstrong;
+{
+public static void main(string args[]);
+{
+int num=1634;
+int n=0;
+int original=num;
+while (original!=0)
+{
+original/=0;
+++n;
+}
+original=number;
+int result=0;
+while(original!=0)
+{
+int remainder=original%10;
+result+=math.pow(remainder.n);
+original/=10;
+}
+if(result==num)
+{
+system.out.println("not an armstrong number");
+}
+}
+}

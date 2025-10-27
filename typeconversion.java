c4a754561f561d92ea7c0de2b26c4b78ca48332a
sai koluguri <luckyizdone@gmail.com>
2025-10-27 11:55:20 +0530

    tc

---
+++ typeconversion.java
@@ -0,0 +1,9 @@
+class typeconvesctions
+{
+ public static void main{string[] args) {
+int discountPercentage = 10i;
+double totalprice = 200;
+double priceAfterDiscount = totalPrice * (1 - (discountPercentage / 100));
+System.out.println("customer has paid a bill of amount: "+ priceAfterDiscount);
+ }
+}

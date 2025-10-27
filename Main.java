ff63bf37e0f174fa223c22f11f8bfea3bc429b90
sai koluguri <luckyizdone@gmail.com>
2025-10-27 11:13:25 +0530

    init commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.io.*;
+class by value;
+{
+void example (int x,int y)
+{
+x++;
+y++;
+}
+}
+class main
+{
+psvm()
+{
+int a=10;
+int b=20;
+cbv c1=new cbv();
+s.o.p("value of a" +a+ "&" +b)
+c1 example(a,b);
+s.o.p("value of a" +a+ "&" +b);
+}
+} 
